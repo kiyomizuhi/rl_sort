af3b016fefb4be4ebb62e600d20d4e94a9f1962c
hiroyukiinoue <hiroyukiinoue1444@gmail.com>
2019-08-07 01:15:44 +0900

    fix the network output size

--- src/network.py
+++ src/network.py
@@ -4,16 +4,16 @@ import chainer.functions as F
 import chainer.links as L
 from config import NUM_SLOTS
 
-a = np.exp(np.log(NUM_SLOTS) / 4)
+a = np.exp(np.log(NUM_SLOTS / 2) / 4)
 layer0 = NUM_SLOTS
 layer1 = int(a * NUM_SLOTS)
 layer2 = int((a ** 2) * NUM_SLOTS)
 layer3 = int((a ** 3) * NUM_SLOTS)
-layer4 = NUM_SLOTS * NUM_SLOTS
+layer4 = int(NUM_SLOTS * (NUM_SLOTS - 1) / 2)
 
 class QNet(Chain):
     def __init__(self):
-        super(MyAE, self).__init__(
+        super(QNet, self).__init__(
             l1=L.Linear(layer0, layer1, initialW=initializers.Normal(scale=0.05)),
             l2=L.Linear(layer1, layer2, initialW=initializers.Normal(scale=0.05)),
             l3=L.Linear(layer2, layer3, initialW=initializers.Normal(scale=0.05)),

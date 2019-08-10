eecdbda005285753109fa6f424051a8c135d7185
hiroyukiinoue <hiroyukiinoue1444@gmail.com>
2019-08-11 02:44:56 +0900

    not learning well

--- src/agent.py
+++ src/agent.py
@@ -1,8 +1,11 @@
 import numpy as np
+import random
+import copy
+from config import NUM_SLOTS
 from abc import ABC, abstractmethod
-from collections import defaultdict
 from network import QNet
-from chainer import serializer
+from chainer import serializer, Variable, optimizers, optimizer_hooks
+import chainer.functions as F
 
 class Agent(ABC):
     """
@@ -21,20 +24,24 @@ class DQNAgent(Agent):
     """
     eps
     """
-    def __init__(self, env, epsilon=0.5, learning_rate=0.1, init_model=False):
+    def __init__(self, env, epsilon=0.5, learning_rate=0.01, init_model=False):
         self.env = env
         self.gamma = 0.95
         self._epsilon = epsilon
         self.actions = env.action_space
         self.learning_rate = learning_rate
-        self.log_experiences = []
-        self.log_episodes = []
-        print(env.render())
+        self.batch_size = 25
+        self.freq_update = 5
+        self.experiences = []
+        env.render()
 
         if init_model:
-            self.QNet = QNet()
+            self.model = QNet()
         else:
-            self.QNet = DQNAgent.load_model() #TODO
+            self.model = DQNAgent.load_model()
+        self.optimizer = optimizers.Adam()
+        self.optimizer.setup(self.model)
+        self.optimizer.add_hook(optimizer_hooks.GradientClipping(1.0))
 
     @property
     def epsilon(self):
@@ -44,46 +51,94 @@ class DQNAgent(Agent):
     def epsilon(self, epsilon):
         self._epsilon = epsilon
 
-    def policy(self, s):
+    def reduce_epsilon(self):
+        self.epsilon = 0.9999 * self.epsilon
+
+    def policy(self, state):
         if np.random.rand() < self._epsilon:
             return np.random.choice(self.actions)
         else:
-            return np.argmax(self.Q[s])
+            Q = self.compute_Q(self.model, state.array)
+            return np.argmax(Q.data)
 
     def init_log_experiences(self):
-        self.log_experiences = []
+        self.experiences = []
 
     def log_experience(self, exp):
-        self.log_experiences.append(exp)
+        self.experiences.append(exp)
 
-    def log_episode(self):
-        self.log_episodes.append(self.log_experiences)
+    def recall_experiences(self):
+        exps = random.sample(self.experiences[:-1], self.batch_size - 1)
+        exps.append(self.experiences[-1])
+        s1s = np.zeros((self.batch_size, NUM_SLOTS))
+        s2s = np.zeros((self.batch_size, NUM_SLOTS))
+        acs = np.zeros(self.batch_size)
+        rws = np.zeros(self.batch_size)
+        exps_samp = random.sample(exps, self.batch_size)
+        for i, exp in enumerate(exps_samp):
+            s1s[i, :] = exp[0].array
+            acs[i] = exp[1]
+            s2s[i, :] = exp[2].array
+            rws[i] = exp[3]
+        acs = acs.astype(int)
+        return s1s, acs, s2s, rws
 
-    def train(self, num_episodes=1000):
-        self.init_Q()
-        self.init_sa_counts()
-        for _ in range(num_episodes):
-            self.init_log_experiences()
-            # self.init_sa_counts()
-            self.train_episode()
-            self.log_episode()
-            self.epsilon = 0.999 * self.epsilon
+    def get_num_eperiences(self):
+        return len(self.experiences)
 
     def train_episode(self):
-        this_s = self.env.reset()
         done = False
+        step = 0
         while not done:
-            a = self.policy(this_s)
-            next_s, reward, done, info = self.env.step(a)
-            reward = self.env.dict_state_reward[next_s]
-            self.update_Q(this_s, a, reward)
-            experience = (this_s, next_s, a, reward, done)
+            action = self.policy(self.env.state_prst)
+            state_next, reward, done = self.env.step(action)
+            experience = (self.env.state_prst, action, state_next, reward, done)
             self.log_experience(experience)
-            this_s = next_s
+            if (self.get_num_eperiences() > self.batch_size) and\
+                step % self.freq_update == 0:
+                s1s, acs, s2s, rws = self.recall_experiences()
+                self.update_Q(s1s, acs, s2s, rws)
+                self.reduce_epsilon()
+            self.env.state_prst = state_next
+            if step == 100000:
+                break
+            step += 1
+        return self.experiences
 
-    def update_Q(self, s, a, r):
-        gain = r + self.gamma * max(self.Q[s])
-        self.Q[s][a] += self.learning_rate * (gain - self.Q[s][a])
+    def update_Q(self, s1s, acs, s2s, rws):
+        Q_prst = self.compute_Q(self.model, s1s)
+        Q_next = self.compute_Q(self.model, s2s)
+        target = copy.deepcopy(Q_next.data)
+        target[np.arange(self.batch_size), acs] = rws + self.gamma * Q_next.data.max(axis=1)
+        target = Variable(target.astype(np.float32))
+        self.model.cleargrads()
+        loss = F.mean_squared_error(Q_prst, target)
+        loss.backward()
+        self.optimizer.update()
+
+    def compute_Q(self, model, state):
+        features = self.feature_engineering(state)
+        features = Variable(features.astype(np.float32))
+        Q = model.fwd(features)
+        return Q
+
+    def feature_engineering(self, state):
+        if state.ndim == 1:
+            min_state = state.min()
+            max_state = state.max()
+            if max_state == min_state:
+                delta = 1
+            else:
+                delta = max_state - min_state
+            features = (state - min_state) / delta
+            return features[np.newaxis, :]
+        elif state.ndim == 2:
+            min_state = state.min(axis=1, keepdims=True)
+            max_state = state.max(axis=1, keepdims=True)
+            delta = max_state - min_state
+            delta[delta == 0] = 1.0
+            features = (state - min_state) / delta
+            return features
 
     @staticmethod
     def save_model(self):
@@ -95,14 +150,5 @@ class DQNAgent(Agent):
 
 
 
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    from env_config import *
-    env = Environment(grid)
-    agent = RandomAgent(env)
+# if __name__ == '__main__':
+#     pass
--- src/environment.py
+++ src/environment.py
@@ -2,34 +2,30 @@ import enum
 import numpy as np
 import copy
 from config import NUM_SLOTS
-from action import generate_dict_action_slot_pair
+from action_slotpair import generate_dict_action_slotpair
 
-# class Action(enum.Enum):
-#     UP = 0
-#     LEFT = 1
-#     DOWN = 2
-#     RIGHT = 3
 
 class State():
     def __init__(self, array):
         self.array = array
 
     def __repr__(self):
-        str_ = ', '.join([f'{arr}' for arr in self.array])
-        return f"<State: [{str_}]"
+        str_ = ', '.join([str(arr) for arr in self.array])
+        return f"<State: [{str_}]>"
 
     def clone(self):
-        return State(self.array)
+        array = copy.deepcopy(self.array)
+        return State(array)
 
     def __hash__(self):
         return hash(tuple(self.array))
 
-    def __eq__(self, array):
-        return all([arr1 == arr2 for arr1, arr2 in zip(self.array, array)])
+    def __eq__(self, state):
+        return (self.array == state.array).all()
 
-    def swap_slots(self, slot_pair):
-        s1, s2 = slot_pair
+    def swap_pair(self, slot_pair):
         st = self.clone()
+        s1, s2 = slot_pair
         st.array[s2], st.array[s1] = st.array[s1], st.array[s2]
         return st
 
@@ -42,15 +38,14 @@ class Environment():
         - reward_function: state, state' -> reward
     """
     def __init__(self, array):
-        self.dict_action_slotpair, self.dict_slotpair_action = generate_dict_action_slot_pair()
+        self.dict_action_slotpair, self.dict_slotpair_action = generate_dict_action_slotpair()
         self.action_space = list(self.dict_action_slotpair.keys())
         self._state_init = State(array)
-        self.state_prst = State(array)
         self.default_reward = -0.05
         self.reset()
 
     def render(self):
-        self.state_prst.__repr__()
+        print(self.state_prst)
 
     @property
     def state_init(self):
@@ -58,33 +53,38 @@ class Environment():
 
     def reset(self):
         self.state_prst = self._state_init.clone()
-        return self.state_prst
 
     def step(self, action):
         slot_pair = self.dict_action_slotpair[action]
-        state_next = self.state_prst.swap_slots(slot_pair)
+        state_next = self.state_prst.swap_pair(slot_pair)
         reward, done = self.reward_func(self.state_prst, state_next)
-        if state_next is not None:
-            self.state_prst = state_next
         return state_next, reward, done
 
     def reward_func(self, s1, s2):
         # in general, reward func depends both on
         # current state and next state
         reward = self.default_reward
-        score1, score2 = Environment.eval_array_scores([s1, s2])
+        score1, score2 = Environment.eval_state_scores([s1, s2])
         if score2 == NUM_SLOTS - 1:
             done = True
-            reward = 10
+            reward += 2
         else:
             done = False
-            if score1 > score2:
+            if score1 < score2:
                 reward += 1
             elif score1 == score2:
                 reward += 0
             else:
                 reward += -1
         return reward, done
+
+    @staticmethod
+    def eval_state_scores(states):
+        return [sum(Environment.eval_array(st.array)) for st in states]
+
+    @staticmethod
+    def eval_array(array):
+        return [Environment.eval_order(array[i], array[i + 1]) for i in range(len(array) - 1)]
 
     @staticmethod
     def eval_order(s1, s2):
@@ -94,11 +94,3 @@ class Environment():
             return 0
         else:
             return -1
-
-    @staticmethod
-    def eval_array(array):
-        return [Environment.eval_order(array[i], array[i + 1]) for i in range(len(array) - 1)]
-
-    @staticmethod
-    def eval_array_scores(arrays):
-        return [sum(Environment.eval_array(array)) for array in arrays]
